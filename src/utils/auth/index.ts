9987507b410736a586b2cc539313b0da9dea9290
ninjawarrior1337 <me@treelar.xyz>
2025-03-29 22:38:54 -0700

    feat: one of the most cursed functions I've ever written

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,3 +1,6 @@
+import { getDefaultStore } from "jotai";
+import { resetAuthState, tokenAtom } from "./store";
+
 export const getToken = async ({
   username,
   password,
@@ -15,20 +18,87 @@ export const getToken = async ({
   });
 
   if (!res.ok) {
-    const err = (await res.json()) as {detail: string}
+    const err = (await res.json()) as { detail: string };
     throw Error(err.detail);
   }
 
-  return (await res.json()) as {
+  const data = (await res.json()) as {
     access_token: string;
     token_type: "bearer";
   };
+
+  const store = getDefaultStore();
+  store.set(tokenAtom, data.access_token);
 };
 
-export const getUserInfo = async (token: string) => {
+export const refreshToken = async () => {
+  const store = getDefaultStore();
+  const res = await fetch("/api/auth/refresh", {
+    method: "POST",
+  });
+
+  if (!res.ok) {
+    resetAuthState();
+    return;
+  }
+
+  const token = (await res.json()) as {
+    access_token: string;
+    token_type: "bearer";
+  };
+
+  store.set(tokenAtom, token.access_token);
+  return token;
+};
+
+export const getAuthenticatedRoute =
+  <T>(f: (token: string | undefined) => Promise<T>): (() => Promise<T>) =>
+  async () => {
+    const store = getDefaultStore();
+    const accessToken = store.get(tokenAtom);
+
+    try {
+      return f(accessToken);
+    } catch {
+      const newToken = await refreshToken();
+      if (!newToken) {
+        throw new Error("Unauthenticated");
+      }
+      return f(newToken.access_token);
+    }
+  };
+
+export const getAccessToken = async (): Promise<string> => {
+  const store = getDefaultStore();
+  const accessToken = store.get(tokenAtom);
+
+  // Try to use the token, if invalid throw
   const res = await fetch("/api/auth/users/me", {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (res.status == 401) {
+    const newToken = await refreshToken();
+    if (!newToken) {
+      throw new Error("Unauthenticated");
+    }
+    return getAccessToken();
+  }
+
+  return accessToken!;
+};
+
+export const logout = async () => {
+  await fetch("/api/auth/logout", { method: "POST" });
+  resetAuthState();
+};
+
+export const getUserInfo = getAuthenticatedRoute(async (t) => {
+  const res = await fetch("/api/auth/users/me", {
+    headers: {
+      Authorization: `Bearer ${t}`,
     },
   });
 
@@ -40,4 +110,4 @@ export const getUserInfo = async (token: string) => {
     name: string;
     email: string;
   };
-};
+});
