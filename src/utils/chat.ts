9b55ae0fae4337e3ca04d260c674c0a56625a16c
ninjawarrior1337 <me@treelar.xyz>
2025-04-24 13:29:06 -0700

    i realized that if u wanted consistent behavior, you would have to use some form of jotai anyway

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -2,8 +2,12 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useMemo, useRef, useState } from "react";
 import { ulid } from "ulid";
 import { z } from "zod";
-import { appendMessageToChatThreadById, getChatThreadById, renameThreadById } from "./chatHistory";
-import { useAtomValue } from "jotai";
+import {
+  appendMessageToChatThreadById,
+  getChatThreadById,
+  renameThreadById,
+} from "./chatHistory";
+import { atom, getDefaultStore, useAtomValue } from "jotai";
 import { isAuthenticatedAtom } from "./auth/store";
 
 export const zChatMessage = z.object({
@@ -28,80 +32,84 @@ export function useTextStream({
 }: {
   addToHistory: (cm: ChatMessage) => void;
 }) {
-  const [currentUlid, setULID] = useState(ulid())
-  const [streamingString, setStreamingString] = useState<string[]>([])
+  const ulidAtom = useMemo(() => atom(ulid()), []);
+  const streamingStringAtom = useMemo(() => atom<string[]>([]), []);
 
-  const streamingMessage = useMemo<ChatMessage>(() => {
-    return {
-      message: streamingString.join(""),
-      role: "assistant",
-      id: currentUlid
-    }
-  }, [streamingString, currentUlid])
+  const streamingMessageAtom = useMemo(
+    () =>
+      atom<ChatMessage>((get) => ({
+        id: get(ulidAtom),
+        message: get(streamingStringAtom).join(""),
+        role: "assistant",
+      })),
+    [ulidAtom, streamingStringAtom],
+  );
+  const streamingMessage = useAtomValue(streamingMessageAtom);
 
-  const streamingMessageRef = useRef(streamingMessage)
-  streamingMessageRef.current = streamingMessage
-
-  const [isStreaming, setIsStreaming] = useState(false)
+  const [isStreaming, setIsStreaming] = useState(false);
 
   const start = () => {
-    setULID(() => ulid())
-    setIsStreaming(true)
-  }
+    const store = getDefaultStore();
+    store.set(ulidAtom, () => ulid());
+    setIsStreaming(true);
+  };
 
   const sendToken = (s: string) => {
-    setStreamingString((sm) => [...sm, s]);
+    const store = getDefaultStore();
+    store.set(streamingStringAtom, (sm) => [...sm, s]);
   };
 
   const end = () => {
-    // We must do this since this function is called outside of React in a promise callback
-    addToHistory(streamingMessageRef.current);
-    setStreamingString([]);
-    setIsStreaming(false)
+    const store = getDefaultStore();
+    addToHistory(store.get(streamingMessageAtom));
+    store.set(streamingStringAtom, []);
+    setIsStreaming(false);
   };
 
-  return {sendToken, start, end, streamingMessage, isStreaming} as const;
+  return { sendToken, start, end, streamingMessage, isStreaming } as const;
 }
 
 export function useChatHistory(chatThreadId?: string) {
-  const qc = useQueryClient()
-  const chatThreadExists = useRef(false)
-  const isAuthenticated = useAtomValue(isAuthenticatedAtom)
+  const qc = useQueryClient();
+  const chatThreadExists = useRef(false);
+  const isAuthenticated = useAtomValue(isAuthenticatedAtom);
 
   const [history, setHistory] = useState([] as ChatMessage[]);
   const [prevThreadId, setPrevTreadId] = useState(chatThreadId);
 
-  if(chatThreadId !== prevThreadId) {
-    setPrevTreadId(() => chatThreadId)
+  if (chatThreadId !== prevThreadId) {
+    setPrevTreadId(() => chatThreadId);
 
-    if(chatThreadId) {
-      getChatThreadById({id: chatThreadId}).then(ct => {
-        setHistory(ct.thread)
-        chatThreadExists.current = true
-      }).catch(() => {
-        setHistory([])
-      })
+    if (chatThreadId) {
+      getChatThreadById({ id: chatThreadId })
+        .then((ct) => {
+          setHistory(ct.thread);
+          chatThreadExists.current = true;
+        })
+        .catch(() => {
+          setHistory([]);
+        });
     }
   }
 
   const commitMessageToServer = async (cm: StableChatMessage) => {
-    if(chatThreadId && isAuthenticated) {
+    if (chatThreadId && isAuthenticated) {
       await appendMessageToChatThreadById({
         chat_thread_id: chatThreadId,
-        message: cm
-      })
+        message: cm,
+      });
 
-      if(!chatThreadExists.current) {
-        await renameThreadById({id: chatThreadId, name: cm.message})
-        qc.invalidateQueries({queryKey: ["chatHistory"]})
-        chatThreadExists.current = true
+      if (!chatThreadExists.current) {
+        await renameThreadById({ id: chatThreadId, name: cm.message });
+        qc.invalidateQueries({ queryKey: ["chatHistory"] });
+        chatThreadExists.current = true;
       }
     }
-  }
+  };
 
   const addToHistory = (cm: ChatMessage) => {
     setHistory((v) => [...v, cm]);
-    commitMessageToServer(cm as StableChatMessage)
+    commitMessageToServer(cm as StableChatMessage);
   };
 
   return { history, addToHistory };
